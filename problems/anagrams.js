abc8e64dbfe3917f90e16caa042f8e5a10ad8c7e
Chase <chasevanhaselen@gmail.com>
2022-04-29 18:31:09 -0500

    updated algo

--- problems/anagrams.js
+++ problems/anagrams.js
@@ -1,6 +1,8 @@
 // given two strings, write a function to determine if the second string is an anagram of the first.  an anagram is a word, phrase, or name, formed by rearranging the letters of another, such as "cinema", formed from "iceman"
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  if (str1 === "" || str2 === "") return true;
   const anagram = {};
 
   for (let i = 0; i < str1.length; i++) {
@@ -18,10 +20,10 @@ function isAnagram(str1, str2) {
       anagram[char] -= 1
     }
   }
-  if (!Object.values(anagram).reduce((a, b) => a + b)) {
-    return true;
-  }
+  return true;
+  
 }
 
 console.log(isAnagram("iceman", "cinema"));
 console.log(isAnagram("iceman", "franks"));
+console.log(isAnagram("", "yo"));
