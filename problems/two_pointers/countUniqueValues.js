3c5ccf42ac433c35165409ada585a12d56523c76
Chase <chasevanhaselen@gmail.com>
2022-05-01 15:28:24 -0500

    uniqueChars with comments

--- problems/two_pointers/countUniqueValues.js
+++ problems/two_pointers/countUniqueValues.js
@@ -41,17 +41,35 @@ function countUniqueValues(arr) {
   // }
   // return count
 
-  var i = 0;
-  for (var j = 1; j < arr.length; j++) {
+  // input: a sorted array
+  // output: a number representing the count of unique characters within the array
+  // example: [1, 1, 1] => 2 (because 1 is counted once, and 2 is counted once; thus, 2 is the final output)
+  // plan:  two pointers
+  // 1. set pointer 1 = 0 to initiate start of streak
+  // 2. set pointer 2 inside a for loop
+  // 3. loop through array
+  // 4. if pointer 1's char === pointer 2's char, don't do anything
+  // 5. if pointer 1's char !== pointer 2's char, that ends our streak of a unique chars, so we need to increment pointer 1 (as this represents our count)
+  // 6. repeat loop until j <= arr.length;
+  // 7. return i at end of loop
+
+  let i = 0;
+  for (let j = 1; j <= arr.length; j++) {
     if (arr[i] !== arr[j]) {
-      i++;
-      arr[i] = arr[j];
+      i++
+      arr[i] = arr[j] // why is this necessary?  because by setting i = j, i keep a reference of the start of a new streak if i's current streak isn't over yet and j's now on a new streak => i have to reset i to be on j's new streak or else i'll just keep counting the same streak
     }
   }
-  return i + 1;
+  return i;
 }
-//                      i   j
-console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
-console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
-console.log(countUniqueValues([1, 2, 3, 4, 5, 6, 7])); // 7
-console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+
+
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 5, 5, 5])); // 5
+
+
+
+
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2])); // [1, ]
+// console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
+// console.log(countUniqueValues([1, 2, 3, 4, 5, 6, 7])); // 7
+// console.log(countUniqueValues([-2, -1, 0, 1, 1]))
